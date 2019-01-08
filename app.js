83d82dd35db0d1ce5037bb6f82d25251fe554c4f
octoccoper <octocopper@gmail.com>
2019-01-08 20:43:55 +0200

    added initial code to app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById("button1").addEventListener("click",getText);
+document.getElementById("button2").addEventListener("click",getJSON);
+document.getElementById("button3").addEventListener("click",getAPIdata);
+
