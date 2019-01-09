810eeddc3b0632838739014ba57ba860dcc187fe
octoccoper <octocopper@gmail.com>
2019-01-09 11:37:46 +0200

    modified app.js: changed getAPIdata() and getJSON()

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ document.getElementById("button1").addEventListener("click",getText);
 document.getElementById("button2").addEventListener("click",getJSON);
 document.getElementById("button3").addEventListener("click",getAPIdata);
 
+// Get local test.txt file
 function getText() { 
   fetch("test.txt")
     .then(function (res) {
@@ -15,26 +16,38 @@ function getText() {
     })
 }
 
+// Get local JSON data
 function getJSON() { 
-  fetch("test.txt")
+  fetch("posts.json")
     .then(function (res) {
-      return res.text();
+      return res.json();
     })
     .then(function (data) {
-      document.getElementById("output").innerHTML = data;
+      let resultData = "";
+      data.forEach(element => {
+        resultData += `<li>${element.title}</li>`;
+      });
+
+      document.getElementById("output").innerHTML = resultData;
     })
     .catch(function(err){
       console.log(err);
     })
 }
 
+// Get API data
 function getAPIdata() { 
-  fetch("test.txt")
+  fetch("https://api.github.com/users")
     .then(function (res) {
-      return res.text();
+      return res.json();
     })
     .then(function (data) {
-      document.getElementById("output").innerHTML = data;
+      let resultData = "";
+      data.forEach(element => {
+        resultData += `<li>${element.login}</li>`;
+      });
+
+      document.getElementById("output").innerHTML = resultData;
     })
     .catch(function(err){
       console.log(err);
