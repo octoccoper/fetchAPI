0bc90304203252a525965a1cbecaffb8d806d84d
octoccoper <octocopper@gmail.com>
2019-01-09 11:27:01 +0200

    updated functions in app.js

--- app.js
+++ app.js
@@ -2,3 +2,41 @@ document.getElementById("button1").addEventListener("click",getText);
 document.getElementById("button2").addEventListener("click",getJSON);
 document.getElementById("button3").addEventListener("click",getAPIdata);
 
+function getText() { 
+  fetch("test.txt")
+    .then(function (res) {
+      return res.text();
+    })
+    .then(function (data) {
+      document.getElementById("output").innerHTML = data;
+    })
+    .catch(function(err){
+      console.log(err);
+    })
+}
+
+function getJSON() { 
+  fetch("test.txt")
+    .then(function (res) {
+      return res.text();
+    })
+    .then(function (data) {
+      document.getElementById("output").innerHTML = data;
+    })
+    .catch(function(err){
+      console.log(err);
+    })
+}
+
+function getAPIdata() { 
+  fetch("test.txt")
+    .then(function (res) {
+      return res.text();
+    })
+    .then(function (data) {
+      document.getElementById("output").innerHTML = data;
+    })
+    .catch(function(err){
+      console.log(err);
+    })
+}
