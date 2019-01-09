adc2fe1c383fc0830a3727799d9a68f44db904c0
octoccoper <octocopper@gmail.com>
2019-01-09 18:35:52 +0200

    modified app.js for using arrow functions instead of standard

--- app.js
+++ app.js
@@ -5,51 +5,36 @@ document.getElementById("button3").addEventListener("click",getAPIdata);
 // Get local test.txt file
 function getText() { 
   fetch("test.txt")
-    .then(function (res) {
-      return res.text();
-    })
-    .then(function (data) {
-      document.getElementById("output").innerHTML = data;
-    })
-    .catch(function(err){
-      console.log(err);
-    })
+    .then(res => res.text())
+    .then(data => document.getElementById("output").innerHTML = data)
+    .catch(err =>console.log(err));
 }
 
 // Get local JSON data
 function getJSON() { 
   fetch("posts.json")
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (data) {
+    .then(res => res.json())
+    .then(data => {
       let resultData = "";
       data.forEach(element => {
         resultData += `<li>${element.title}</li>`;
-      });
-
+      })
       document.getElementById("output").innerHTML = resultData;
     })
-    .catch(function(err){
-      console.log(err);
-    })
+    .catch(err => console.log(err))
 }
 
 // Get API data
 function getAPIdata() { 
   fetch("https://api.github.com/users")
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (data) {
+    .then(res => res.json())
+    .then(data => {
       let resultData = "";
       data.forEach(element => {
         resultData += `<li>${element.login}</li>`;
       });
-
       document.getElementById("output").innerHTML = resultData;
     })
-    .catch(function(err){
-      console.log(err);
-    })
+
+    .catch(err =>  console.log(err))
 }
